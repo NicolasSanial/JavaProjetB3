8e2cdeae8d9cc79f4a5b620ffe0730386f38f3f4
nisan <nicolas.sanial@ecoles-oss.net>
2018-01-11 18:27:32 +0100

    Cote page connexion, utilisation des fichiers xml et connexion avec la vue et implémentation du controller

--- src/packageConnection/ConnectionController.java
+++ src/packageConnection/ConnectionController.java
@@ -1,9 +1,23 @@
 package packageConnection;
 
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import packageAdmin.AdminView;
+
 public class ConnectionController {
 
-    public void close(){
-
+    /* Action of the page administration button */
+    @FXML
+    private void goPageAdminButtonAction() {
+        Stage stage = new Stage();
+        new AdminView().start(stage);
     }
 
+    /* Action of the close button */
+    @FXML private javafx.scene.control.Button closeButton;
+    @FXML
+    private void closeButtonAction() {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
+    }
 }
--- src/packageConnection/ConnectionView.java
+++ src/packageConnection/ConnectionView.java
@@ -1,35 +1,51 @@
 package packageConnection;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.Group;
-import javafx.scene.paint.Color;
-import javafx.scene.control.Button;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import packageAdmin.AdminView;
+
+import java.io.IOException;
 
 
 public class ConnectionView extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    private Stage connectionStage;
 
     /* Method who configure the initialisation of the connection view */
     @Override
     public void start(Stage connectionStage) throws Exception{
-        connectionStage.setTitle("PDF-Stocker");
-        Group connectionGroup = new Group();
-        Scene connectionScene = new Scene(connectionGroup, 600, 450, Color.LIGHTGREY);
 
+        this.connectionStage = connectionStage;
+
+        initConnectionView();
+
+    }
+
+    public void initConnectionView() {
+        try {
+            // Load root layout from fxml file.
+            Parent root = FXMLLoader.load(getClass().getResource("ConnectionViewXML.fxml"));
+            Scene connectionScene = new Scene(root);
+
+            connectionStage.setScene(connectionScene);
+            connectionStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+        /*
         Button btnAdmin = new Button();
         btnAdmin.setLayoutX(450);
         btnAdmin.setLayoutY(400);
         btnAdmin.setText("Administration");
+        */
 
-        /* Action of the page administration button (TO DO BETTER : event linked with the controller)*/
+        /*
         btnAdmin.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 Stage stage = new Stage();
@@ -42,18 +58,13 @@ public class ConnectionView extends Application {
         btnQuit.setLayoutX(50);
         btnQuit.setLayoutY(400);
         btnQuit.setText("Quitter");
-
+        */
         /* Action of the leave button (TO DO BETTER : event linked with the controller)*/
+        /*
         btnQuit.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 connectionStage.close();
             }
         });
-
+        */
         /* fix all componants on the view */
-        connectionGroup.getChildren().add(btnQuit);
-        connectionGroup.getChildren().add(btnAdmin);
-        connectionStage.setScene(connectionScene);
-        connectionStage.show();
-    }
-}
