1ef6c85afa7dc5ce411b99e9836c0c359c96e771
John-Loup Pautard <johnloup.pautard@mail.com>
2018-01-26 21:19:48 +0100

    modif connection

--- src/packageMain/JDBC.java
+++ src/packageMain/JDBC.java
@@ -7,32 +7,40 @@ import java.sql.Statement;
 import java.sql.ResultSet;
 
 
-public class HandleConnectionDB {
+public class JDBC {
 
     private static Connection con = null;
+    private static JDBC instance;
+    final static String DRIVER = "com.mysql.jdbc.Driver";
+    final static String URL = "";
+    final static String LOGIN = "";
+    final static String PWD = "";
 
-    private static HandleConnectionDB instance = new HandleConnectionDB();
-
-    public HandleConnectionDB() {
-
-    }
-
-
-    public static HandleConnectionDB getInstance() {
-
-        return HandleConnectionDB.instance;
-    }
-
-
-    public static Connection getConnection() {
+    public JDBC() {
 
         try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost/" + "user=root&password=root");
+            Class.forName(DRIVER);
+            con = DriverManager.getConnection(URL, LOGIN, PWD);
         } catch (SQLException ex) {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
+            System.exit(-1);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.exit(-1);
         }
+    }
+
+
+    public static JDBC getInstance() {
+
+        if (instance == null) instance = new JDBC();
+        return instance;
+    }
+
+
+    public static Connection getConnection() {
 
         return con;
     }
@@ -43,7 +51,7 @@ public class HandleConnectionDB {
         if (con != null) {
             try {
                 con.close();
-            } catch (SQLException e) { /* ignored */}
+            } catch (SQLException ex) {/* ignored */}
         }
 
         return con;
@@ -52,11 +60,10 @@ public class HandleConnectionDB {
 
     public static ResultSet executeQuery(String query) {
 
-        Connection con = null;
         Statement stmt = null;
         ResultSet rs = null;
 
-        con = HandleConnectionDB.getConnection();
+        con = JDBC.getConnection();
 
         // Create statement
         try {
@@ -65,6 +72,7 @@ public class HandleConnectionDB {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
+            System.exit(-1);
         }
         // Execute query
         try {
@@ -73,6 +81,7 @@ public class HandleConnectionDB {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
+            System.exit(-1);
         }
 
         return rs;
--- src/packageModels/UserGestionList.java
+++ src/packageModels/UserGestionList.java
@@ -11,6 +11,7 @@ public class UserGestionList {
      * Constructor for the singleton
      */
     public UserGestionList() {
+        super();
         loadUserintoList();
     }
 
